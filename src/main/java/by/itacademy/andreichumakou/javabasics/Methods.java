302886b37152abfa82468d415987f20617c85a55
Andrei Chumakou <chumakou.andrei@gmail.com>
2023-02-28 09:50:26 +0300

    refactor Methods.java

--- src/main/java/by/itacademy/andreichumakou/javabasics/Methods.java
+++ src/main/java/by/itacademy/andreichumakou/javabasics/Methods.java
@@ -8,9 +8,13 @@ public class Methods {
         } else return quantityNews / 10 + 1;
     }
 
-    public int displayNumber(int quantityNumbers) {
+    public int printNumber(int quantityNumbers) {
         if (quantityNumbers <= 0) {
             throw new IllegalArgumentException("Wrong number!");
         } else return (int) (Math.random() * Math.pow(10, quantityNumbers));
+    }
+
+    public static double printAreaCircle(double radius) {
+        return Math.PI * Math.pow(radius, 2);
     }
 }
