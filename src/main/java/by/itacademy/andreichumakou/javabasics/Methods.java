13f8084f8dbb854f132332fd35ac4c7dbc19d84c
Andrei Chumakou <chumakou.andrei@gmail.com>
2023-02-28 08:41:14 +0300

    refactor Methods.java add Exception

--- src/main/java/by/itacademy/andreichumakou/javabasics/Methods.java
+++ src/main/java/by/itacademy/andreichumakou/javabasics/Methods.java
@@ -3,10 +3,14 @@ package by.itacademy.andreichumakou.javabasics;
 public class Methods {
 
     public int countQuantityNews(int quantityNews) {
-        return quantityNews / 10 + 1;
+        if (quantityNews < 0) {
+            throw new IllegalArgumentException("Wrong number!");
+        } else return quantityNews / 10 + 1;
     }
 
     public int displayNumber(int quantityNumbers) {
-        return (int) (Math.random() * Math.pow(10, quantityNumbers));
+        if (quantityNumbers <= 0) {
+            throw new IllegalArgumentException("Wrong number!");
+        } else return (int) (Math.random() * Math.pow(10, quantityNumbers));
     }
 }
