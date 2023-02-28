722ebcfd86da834822651e30e75da7321911055b
Andrei Chumakou <chumakou.andrei@gmail.com>
2023-02-28 08:38:22 +0300

    refactor Methods.java add displayNumber()

--- src/main/java/by/itacademy/andreichumakou/javabasics/Methods.java
+++ src/main/java/by/itacademy/andreichumakou/javabasics/Methods.java
@@ -5,4 +5,8 @@ public class Methods {
     public int countQuantityNews(int quantityNews) {
         return quantityNews / 10 + 1;
     }
+
+    public int displayNumber(int quantityNumbers) {
+        return (int) (Math.random() * Math.pow(10, quantityNumbers));
+    }
 }
