4ad9029954e53329cbb9385a3e12b435247df273
Andrei Chumakou <chumakou.andrei@gmail.com>
2023-02-28 09:50:00 +0300

    add CommandLineParams.java

---
+++ src/main/java/by/itacademy/andreichumakou/javabasics/CommandLineParams.java
@@ -0,0 +1,9 @@
+package by.itacademy.andreichumakou.javabasics;
+
+public class CommandLineParams {
+    public static void main(String[] args) {
+        for(int i = args.length-1; i >= 0; i--) {
+            System.out.println("Argument " + i + " = " + args[i]);
+        }
+    }
+}
